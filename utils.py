f453f09deca6d2189a925decf9fb7a80f26b6b02
CubicPill <huazhengchang97@gmail.com>
2018-09-20 15:51:15 +0800

    Initial commit

---
+++ utils.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def login(username, password):
+    url = 'http://bt.neu6.edu.cn'
+    session = requests.session()
+    response = session.get(url)
+
+    soup = BeautifulSoup(response.content, 'html5lib')
+    form_data = dict()
+    form_element = soup.find('form', {'name': 'login'})
+    for element in form_element.find_all('input'):
+        if element.has_attr('name'):
+            value = ''
+            if element.has_attr('value'):
+                value = element['value']
+            form_data[element['name']] = value
+    form_data['username'] = username
+    form_data['password'] = password
+    response = session.post(url + '/' + form_element['action'], data=form_data)
+    print(response.history)
+    return session
