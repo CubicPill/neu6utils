8fb362c0c095305f46093a16b5c6ce4145d2c1e3
CubicPill <cubicpill@gmail.com>
2018-09-21 15:29:55 +0800

    upload

--- analyze_uploads.py
+++ analyze_uploads.py
@@ -1,2 +1,14 @@
+import random
+import sys
+import time
+
+from bs4 import BeautifulSoup
+
+from utils import login
+
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) != 3:
+        print('Usage: python3 ' + sys.argv[0] + ' <username> <password>')
+        sys.exit(1)
+    username, password = sys.argv[1:]
+    session = login(username, password)
