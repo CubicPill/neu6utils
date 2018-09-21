b1c6b47f25bded1f740c98ee0dbc8ddaba961a49
CubicPill <cubicpill@gmail.com>
2018-09-21 14:57:37 +0800

    fix bug

--- keep_online.py
+++ keep_online.py
@@ -17,15 +17,16 @@ class Worker:
         response = self._session.get(url)
         soup = BeautifulSoup(response.content, 'html5lib')
         forum_urls = [td.find('dt').find('a')['href'] for td in soup.find_all('td', {'class': 'fl_g'})]
-        return ['http://bt.neu6.edu.cn/' + url for url in forum_urls][5:-8]
+        return ['http://bt.neu6.edu.cn/' + url for url in forum_urls][5:-9]
         # get rid of the non-resource forums
 
     def random_access_post(self, form_url):
         response = self._session.get(form_url)
         soup = BeautifulSoup(response.content, 'html5lib')
-        post_urls = ['http://bt.neu6.edu.cn/' + td.find('a', {'class': 's xst'})['href'] for td in
-                     soup.find_all('th', {'class': 'new'})]
-
+        post_urls = ['http://bt.neu6.edu.cn/' + td.find('a', {'class': 'xst'})['href'] for td in
+                     soup.find_all('th', {'class': ['new', 'common']})]
+        if not post_urls:
+            return None
         response = self._session.get(random.choice(post_urls))
         soup = BeautifulSoup(response.content, 'html5lib')
         return soup.find('title').text
@@ -41,8 +42,13 @@ if __name__ == '__main__':
         sys.exit(1)
     worker = Worker(sys.argv[1], sys.argv[2])
     while True:
-        print(time.ctime() + ' Accessed: ' + worker.keep_online())
-        sleep_interval = random.gauss(50, 15)
-        if sleep_interval < 3:
-            sleep_interval = 3
-        time.sleep(sleep_interval)
+        title = worker.keep_online()
+        if title:
+            print(time.ctime() + ' Accessed: ' + title)
+            sleep_interval = random.gauss(50, 15)
+            if sleep_interval < 3:
+                sleep_interval = 3
+            print('Sleep {}s'.format(round(sleep_interval, 2)))
+            time.sleep(sleep_interval)
+        else:
+            print(time.ctime() + ' No posts under this forum, retry')
